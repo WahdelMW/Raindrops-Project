698e6c26b7f434d15955cc5a2509aade6f14735d
WahdelMW <WWoodhouse@spartaglobal.com>
2021-03-29 22:35:29 +0100

    Refactored code

--- src/main/java/com/sparta/wahdel/algorithms/RaindropCalculator.java
+++ src/main/java/com/sparta/wahdel/algorithms/RaindropCalculator.java
@@ -4,22 +4,29 @@ public class RaindropCalculator {
 
     public static String calculate(int number) {
         String result = "";
-        Boolean isRaindrop = false;
-        if (number % 3 == 0) {
+        boolean isRaindrop = false;
+        if (isMultiple(number, 3)) {
             isRaindrop = true;
             result += "Pling";
         }
-        if (number % 5 == 0) {
+
+        if (isMultiple(number, 5)) {
             isRaindrop = true;
             result += "Plang";
         }
-        if (number % 7 == 0) {
+
+        if (isMultiple(number, 7)) {
             isRaindrop = true;
             result += "Plong";
         }
+
         if (!isRaindrop) {
             result = String.valueOf(number);
         }
         return result;
+    }
+
+    private static boolean isMultiple(int number, int i) {
+        return number % i == 0;
     }
 }
--- src/main/java/com/sparta/wahdel/start/Starter.java
+++ src/main/java/com/sparta/wahdel/start/Starter.java
@@ -6,6 +6,9 @@ import com.sparta.wahdel.algorithms.RaindropCalculator;
 public class Starter {
 
     public static void start() {
-        Printer.printMessage(RaindropCalculator.calculate(3));
+        Printer.printMessage(RaindropCalculator.calculate(27));
+        Printer.printMessage(RaindropCalculator.calculate(60));
+        Printer.printMessage(RaindropCalculator.calculate(210));
+        Printer.printMessage(RaindropCalculator.calculate(998));
     }
 }
